7789d7fb1024aa16c7dfd200a531373c292ac5d9
armando.herrera <stickarmand11@gmail.com>
2022-06-11 20:38:24 -0500

    fead:Map.js create

---
+++ map.js
@@ -0,0 +1,9 @@
+// el map te permite iterar sobre un arreglo y modificar sus elementos
+
+// caso de uso: tenemos un array que nos suma la misma cantidad entre si 
+
+const array1 = [1, 4, 9, 16];
+
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
