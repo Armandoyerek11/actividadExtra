87b82e18dfdedf53536716438d485bc33f894429
armando.herrera <stickarmand11@gmail.com>
2022-06-11 21:17:24 -0500

    feat:dindindex.js create

---
+++ findindex.js
@@ -0,0 +1,6 @@
+//este array se utiliza para devolver un numero primo//
+const array1 = [5, 12, 8, 130, 44];
+
+const isLargeNumber = (element) => element > 13;
+
+console.log(array1.findIndex(isLargeNumber));
