b2cb5af887aca52eb7db27b7644c4789a4460e27
armando.herrera <stickarmand11@gmail.com>
2022-06-12 12:19:55 -0500

    feat:promises and hoistinf .js

---
+++ hoisting.js
@@ -0,0 +1,6 @@
+//este metodo de El concepto de Hoisting fue pensado como una manera general de referirse a cómo funcionan los contextos de ejecución en JavaScript (específicamente las fases de creación y ejecución). Sin embargo, el concepto puede ser un poco confuso al principio.//
+frutas.push('Fresa')
+    // ["Manzana", "Banana", "Fresa"]
+
+let pos = frutas.indexOf('Banana') // (pos) es la posición para abreviar
+    // 1
