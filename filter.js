bc906868a5053ab431e7789d6eeb96f5c2be42eb
Armandoyerek11 <75048769+Armandoyerek11@users.noreply.github.com>
2022-06-14 19:46:08 -0500

    Delete filter.js

--- filter.js
+++
@@ -1,7 +0,0 @@
-// filter sirve para filtrar datos //
-// ejemplo aquellos que no cumplan el valor no son filtrados //
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-
-const result = words.filter(word => word.length > 6);
-
-console.log(result);
